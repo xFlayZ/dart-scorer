7a57305dd4bd635757336785d54f62d2f659598c
FlayZ <pierrebrandtner@live.de>
2025-01-11 21:25:23 +0100

    Version-1.6.0-BETA: add new images for gameModes, scores and the new game-settings "difficulty"

--- src/app/dart-preset/dart-preset.component.ts
+++ src/app/dart-preset/dart-preset.component.ts
@@ -1,28 +1,23 @@
-import {
-  Component,
-  EventEmitter,
-  OnInit,
-  Output,
-  ViewChild,
-} from '@angular/core';
+import { Component, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { ChooseSongModalComponent } from '../shared/choose-song-modal/choose-song-modal.component';
 
 interface Player {
-  playerName: string;
+  name: string;
   winnerSong: string;
 }
 
 @Component({
   selector: 'app-dart-preset',
   templateUrl: './dart-preset.component.html',
-  styleUrl: './dart-preset.component.scss',
+  styleUrl: './dart-preset.component.scss'
 })
 export class DartPresetComponent implements OnInit {
   gameModes = [
     { value: 'singleOut', label: 'Single Out', backgroundImage: 'assets/images/single_out.webp' },
     { value: 'doubleOut', label: 'Double Out', backgroundImage: 'assets/images/double_out.webp' },
     { value: 'aroundTheClock', label: 'Around the Clock', backgroundImage: 'assets/images/around_the_clock.webp' },
+    //{ value: 'luckyNumber', label: 'Lucky Number', backgroundImage: 'assets/images/lucky_number.webp' },
   ];
 
   scores = [
@@ -30,8 +25,18 @@ export class DartPresetComponent implements OnInit {
     { value: '501', label: '501', backgroundImage: 'assets/images/score_501.webp' },
   ];
 
+  difficulty = [
+    { value: 'easy', label: 'Einfach', backgroundImage: 'assets/images/easy.png' },
+    //{ value: 'normal', label: 'Einfach', backgroundImage: 'assets/images/normal.png' },
+    //{ value: 'hard', label: 'Einfach', backgroundImage: 'assets/images/hard.png' },
+  ];
+
+  needScore = ['singleOut', 'doubleOut'];
+  needDifficulty = ['aroundTheClock'];
+
   selectedGameMode = 'singleOut';
   selectedScore = '301';
+  selectedDifficulty = 'easy';
 
   isLiteMode: boolean = false;
   players: Player[] = [];
@@ -41,14 +46,10 @@ export class DartPresetComponent implements OnInit {
   gameMode: string = 'singleOut';
   errorMessage: string = '';
   resetButton: boolean = false;
-  @Output() gameStarted: EventEmitter<{
-    players: Player[];
-    scoreValue: string;
-    gameMode: string;
-  }> = new EventEmitter();
+  @Output() gameStarted: EventEmitter<{ players: Player[], scoreValue: string, gameMode: string }> = new EventEmitter();
   @ViewChild(ChooseSongModalComponent) songModal!: ChooseSongModalComponent;
 
-  constructor(private router: Router) {}
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
     const savedPlayers = localStorage.getItem('players');
@@ -66,14 +67,14 @@ export class DartPresetComponent implements OnInit {
     let playerName = this.newPlayerName.trim();
     if (playerName !== '') {
       playerName = playerName.charAt(0).toUpperCase() + playerName.slice(1);
-      this.players.push({ playerName: playerName, winnerSong: 'default' });
-      this.newPlayerName = '';
+      this.players.push({ name: playerName, winnerSong: 'default' });
+      this.newPlayerName = ''; 
       this.errorMessage = '';
       this.resetButton = true;
     }
     localStorage.setItem('players', JSON.stringify(this.players));
   }
-
+  
   removePlayer(index: number): void {
     if (index >= 0 && index < this.players.length) {
       this.players.splice(index, 1);
@@ -85,18 +86,18 @@ export class DartPresetComponent implements OnInit {
   }
 
   updateParticipantsList(): void {
-    const participantsList = document.querySelector('.card-body ul');
-
+    const participantsList = document.querySelector('.card-body ul'); 
+  
     if (participantsList) {
       participantsList.innerHTML = '';
-
-      this.players.forEach((player) => {
-        const listItem = document.createElement('li');
-        listItem.textContent = player.playerName;
+  
+      this.players.forEach(player => {
+        const listItem = document.createElement('li'); 
+        listItem.textContent = player.name; 
         participantsList.appendChild(listItem);
       });
     } else {
-      console.error('Teilnehmerliste nicht gefunden.');
+      console.error('Teilnehmerliste nicht gefunden.'); 
     }
   }
 
@@ -105,20 +106,13 @@ export class DartPresetComponent implements OnInit {
     if (savedPlayers) {
       this.players = JSON.parse(savedPlayers);
     }
-    if (this.players.length > 0) {
-      localStorage.setItem(
-        'gameStartedData',
-        JSON.stringify({
-          players: this.players,
-          scoreValue: this.scoreValue,
-          gameMode: this.gameMode,
-        })
-      );
+    if(this.players.length > 0) {
+      localStorage.setItem('gameStartedData', JSON.stringify({ players: this.players, scoreValue: this.scoreValue, gameMode: this.gameMode }));
       localStorage.removeItem('gameData');
       this.router.navigate([this.gameMode]);
     } else {
-      this.errorMessage = 'Es muss mindestens einen Spieler geben!';
-    }
+      this.errorMessage = "Es muss mindestens einen Spieler geben!";
+    }    
   }
 
   updateScoreValue(scoreValue: string): void {
@@ -130,8 +124,11 @@ export class DartPresetComponent implements OnInit {
   }
 
   resetData(): void {
-    localStorage.clear();
-    window.location.reload();
+    localStorage.removeItem('gameData');
+    localStorage.removeItem('gameStartedData');
+    localStorage.removeItem('players');
+    localStorage.removeItem('speakToTextEnabled');
+    window.location.reload()
   }
 
   openModal(index: number): void {
@@ -141,7 +138,6 @@ export class DartPresetComponent implements OnInit {
   }
 
   capitalizeFirstLetter() {
-    this.newPlayerName =
-      this.newPlayerName.charAt(0).toUpperCase() + this.newPlayerName.slice(1);
+    this.newPlayerName = this.newPlayerName.charAt(0).toUpperCase() + this.newPlayerName.slice(1);
   }
 }
